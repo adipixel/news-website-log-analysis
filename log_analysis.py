897e8ae25e79446a958ebb735eec1beaf81df599
Aditya Mhamunkar <me@adityamhamunkar.com>
2017-07-27 18:39:25 -0400

    Updates after code review

--- log_analysis.py
+++ log_analysis.py
@@ -3,29 +3,21 @@
 # email: me@adityamhamukar.com
 import datetime
 import psycopg2
-import bleach
 
 
 DBNAME = "news"
 
 
 def get_top_three():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
     query = '''select articles.title, counts from articles,
      (select sub.path_slug as paths, count(sub.path_slug) as counts from
      (select substring(path from 10) as path_slug from log where path <> '/')
      as sub GROUP BY sub.path_slug ORDER BY counts desc) as yo
      where articles.slug = yo.paths limit 3;'''
-    c.execute(query)
-    TOP_THREE = c.fetchall()
-    return TOP_THREE
-    db.close()
+    return run_query(query)
 
 
 def get_popular_authors():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
     query = '''select authors.name, sum(final.counts) as sum from authors join
      (select articles.author, counts from articles join
      (select sub.path_slug as paths, count(sub.path_slug) as counts from
@@ -33,24 +25,25 @@ def get_popular_authors():
      as sub GROUP BY sub.path_slug ORDER BY counts desc) as yo
      on articles.slug = yo.paths) as final on
      final.author = authors.id GROUP BY authors.name ORDER BY sum desc;'''
-    c.execute(query)
-    TOP_AUTH = c.fetchall()
-    return TOP_AUTH
-    db.close()
+    return run_query(query)
 
 
 def get_err_status():
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    query2 = '''select to_char(all_status.day, 'Month DD, YYYY') as date,
-     trunc(100*(red_status.count::float/all_status.counts)::numeric,1)
+    query = '''select to_char(all_status.day, 'FMMonth FMDD, YYYY') as date,
+     round(100*(red_status.count::float/all_status.counts)::numeric,1)
      as percent from (select date(time) as day, count(status) from
      log where status LIKE '4%' GROUP BY day) as red_status,
      (select date(time) as day, count(status) as counts from log GROUP BY day)
      as all_status where
      trunc(100*(red_status.count::float/all_status.counts)::numeric,1) > 1
      and all_status.day = red_status.day;'''
-    c.execute(query2)
-    ERR_STATUS = c.fetchall()
-    return ERR_STATUS
+    return run_query(query)
+
+
+def run_query(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
     db.close()
+    return results
